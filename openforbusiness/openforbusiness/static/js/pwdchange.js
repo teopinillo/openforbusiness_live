5dd0270cbd22d5f249afbe5af7b220fabc553a19
Teofilo Francisco <teopinillo@gmail.com>
2022-12-10 04:26:56 +0000

    running

--- openforbusiness/openforbusiness/static/js/pwdchange.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {    
-    document.querySelector('#id_old_password').setAttribute ("class","form-control");
-    document.querySelector('#id_new_password1').setAttribute ("class","form-control");
-    document.querySelector('#id_new_password2').setAttribute ("class","form-control");
-   });
